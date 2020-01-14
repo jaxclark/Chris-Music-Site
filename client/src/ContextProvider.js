957841993d3b749d11eb085653c804d96bbbf7d3
Jax Clark <jamaecla@gmail.com>
2020-01-13 22:21:08 -0700

    update contextprovider

--- client/src/ContextProvider.js
+++ client/src/ContextProvider.js
@@ -25,4 +25,88 @@ function ContextProvider(props) {
                 return res
             })
     }
+    const addMusic = (newMusic) => {
+        return musicAxios.post('/squid/add', newMusic)
+            .then(res => {
+                getStories()
+                return res
+            })
+    }
+    const editMusic = (musicId, music) => {
+        return musicAxios.put(`/squid/add/${musicId}`, music)
+            .then(res => {
+                setMusic(prev => {
+                    const updatedMusic = prev.map(music => {
+                        return music._id === res.data._id ? res.data : music
+                    })
+                    return (updatedMusic)
+                })
+            })
+    }
+    const deleteMusic = (musicId) => {
+        return musicAxios.delete(`/squid/add/${musicId}`)
+            .then(res => {
+                setMusic(prev => {
+                    const updatedMusic = prev.filter(music => {
+                        return music._id !== musicId
+                    })
+                    return (updatedMusic)
+                })
+                return res
+            })
+    }
+
+    const signup = (userInfo) => {
+        console.log('sign provider')
+        return axios.post('/auth/signup', userInfo)
+            .then(response => {
+                const { user, token } = response.data
+                localStorage.setItem('token', token)
+                localStorage.setItem('user', JSON.stringify(user))
+                setUserState(prev => ({
+                    ...prev, user, token
+                }))
+                return response
+            })
+    }
+    const login = (credentials) => {
+        return axios.post('/auth/login', credentials)
+            .then(response => {
+                const { token, user } = response.data
+                localStorage.setItem('token', token)
+                localStorage.setItem('user', JSON.stringify(user))
+                setUserState(prev => ({
+                    ...prev, user, token
+                }))
+                return response
+            })
+    }
+    const logout = () => {
+        localStorage.removeItem('user')
+        localStorage.removeItem('token')
+        setMusic([])
+        setUserState({
+            user: {},
+            token: ''
+        })
+    }
+
+    return(
+        <Context.Provider 
+            value={{
+                ...userState,
+                music,
+                getMusic,
+                addMusic,
+                editMusic,
+                deleteMusic,
+                signup,
+                login,
+                logout
+            }}>
+            { props.children }
+        </Context.Provider>
+    )
 }
+
+export { ContextProvider, Context }
