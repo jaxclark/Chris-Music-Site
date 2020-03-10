e9aeb992a307530ba54f5aab2e9cadd68ff1ef11
Jax Clark <jamaecla@gmail.com>
2020-03-09 20:13:47 -0600

    added info to landing page

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -3,7 +3,43 @@ import React from 'react'
 export default function LandingPage() {
     return(
         <div className='langingPage'>
-
+            <p>
+                <strong>Rhythm Squid</strong><br/>
+                Bio:<br/>
+                Hello! My name is Chris or “Squid” as I’ve been known for a long time (Ask about it when you get in touch, it’s a fun story). My passion for music and the drums have been a guiding force in my life. Whether you’re looking for a solid groove for your own music, wanting to level up your own playing with lessons, or have an event that you want to be unforgettable, you’ve come to the right place. I delight in seeing students grow into their own. Nothing fills me with more joy than backing an incredible band with a great feeling beat. This comes through in your recordings, and at your event when the dance floor is full and bumping. Look around, check out the videos, and get in touch. I’ll look forward to talking with you!<br/>
+                <br/>
+                I have a three main services.<br/>
+                <br/>
+                On one hand I help students achieve their musical goals. By studying with me, you will learn the technical, stylistic, and musical skills necessary to gig out and have a ton of fun along the way.<br/>
+                <br/>
+                On the other hand, I help people with events that need to be memorable bring the party.  By booking one of my bands, you’ll get a variety of songs, styles, and sounds. We specialize in getting people dancing, and singing along, until the wee hours of the night. Get in touch, and learn how we will have people talking about your wedding, corporate party, or convention, for years to come.<br/>
+                <br/>
+                Finally, for my fellow musicians and artists! If you need drums on your record, a last minute fill in on your gig, or a pit musician for your musical production, I’ve done it all. I can read charts if you’ve got them, or fake it if you don’t. Do you play to backing tracks and a click? Perfect! I can steer a band with, or without, a metronome. Please get in touch as soon as you know you need drums, because while these are some of my favorite gigs, my schedule fills up fast and I don’t want to see your time already booked.<br/>
+                <br/>
+                <strong>Reviews:</strong><br/>
+                <em>Grant is a lessons customer. He says:</em><br/>
+                <br/>
+                ”This guy is awesome. Initially hired him to teach my nephew the drums, staring down a costly investment for the kit and everything. He teaches real musicianship and he recommended just doing a few lessons to expose my nephew to different instruments and teaching him music theory. He has a real desire to grow students, and we’re starting with the piano actually because it’s a better method to teach music theory and moving onto the drums a bit later. It saved me some money as well, and I can rest easy that he’ll actually want to play the instruments I invest in. After just a few lessons I can see his understanding of notes, chords, and timing increase dramatically, and the music he mixes has improved as well. Very grateful to see how he approached this. He is skilled, prepared, and organized with a professional curriculum. I’d highly recommend him to anyone wanting to advance in a particular instrument, or music as a whole.”<br/>
+                <br/>
+                <em>Scott S. Is a producer and studio engineer that Chris has worked for. He says:</em><br/>
+                <br/>
+                “Chris is an awesome person to work with and I’m glad I have had the pleasure of doing so. He puts everything he can into whatever project he is working on and is one of the most passionate musicians I know. Not only is Chris extremely knowledgeable but has the talent to match that knowledge. Chris is always on time and ready to go. He is respectful, patient, kind, and very easy going which makes working with him a breeze. Chris has no problem taking feedback or suggestions given to him and implementing them. He will always step up to any challenge given to him. If you are thinking of working with Chris, do yourself a favor and do it.”<br/>
+                <em>Rory is a seasoned and respected member of the Salt Lake Men’s choir and says this about when they hired Chris for a show:</em><br/>
+                <br/>
+                “The Salt Lake Men’s Choir hired the Squid for for drumming for our most popular concert to date. It was an ABBA show featuring a rock band as backup. While I’m not a formal ambassador for the choir, I usually play bass when we need a little low-end help.<br/>
+                The choir isn’t really used to working with instrumentalists, but the Squid was very patient with them and seemed unbothered by the chaos. He was able to learn the parts quickly and accurately, follow our director precisely, and go with the flow. He got along well with everybody, was punctual and professional, and happy to dress in costume for our sold out performance.”<br/>
+                <br/>
+                <em>Sharon is an accomplished pianist, and regular pit musician for the Zigfield Theatre. She says:</em><br/>
+                <br/>
+                “I worked with Chris a couple of times (not his band), he actually took the time to look over and practice the material ahead of time, always came on time and prepared with music and equipment, good at communicating, and he plays with confidence. Always just a good guy easy to talk to and got along with everyone, able to adapt to change; you never hear him complain or speak negatively he’s just ready to go and get the job done and have some fun!”<br/>
+                <br/>
+                <em>Scot F. Has hired Red Shot Pony for his events on a few different occasions. He says:</em><br/>
+                <br/>
+                “I have watched Chris play and his timing is impeccable as well as his Dynamics. Chris is professional and always on time and prepared for rehearsals and gigs.”<br/>
+                <br/>
+                Kopo is a talented multi instrumentalist who has hired Chris both for live shows, and for studio work. He says:<br/>
+                “Chris is a hard working musician. He has a huge passion for his work. He is a master of his art. Practicing until it’s perfect. He knows a lot about his instrument. Not just taking care of them. Tuning and upkeep. But also a very professional performer. When it comes to teaching he is the man. This guy has a huge heart and is all in.”
+            </p>
         </div>
     )
 }
